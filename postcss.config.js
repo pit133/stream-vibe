e6027bb1b77b5b8e836dd29a2bd23089c0cc992d
Petr <117358265+pit133@users.noreply.github.com>
2026-01-17 16:18:24 +0700

    Add help-functions for css constatns

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,17 @@
 import postcssPxToRem from "postcss-pxtorem";
 
 export default ({env}) => {
-    const isProd = env === 'production';
-    const plugins = []
+  const isProd = env === 'production';
+  const plugins = []
 
-    if (isProd) {
-        plugins.push(
-            postcssPxToRem({
-                propList: ['*'],
-                mediaQuery: true,
-            })
-        )
-    }
+  if (isProd) {
+    plugins.push(
+      postcssPxToRem({
+        propList: ['*'],
+        mediaQuery: true,
+      })
+    )
+  }
 
-    return {plugins}
+  return {plugins}
 }
