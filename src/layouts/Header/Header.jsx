c9654b19952d7a16344cf77b46afff409acde8c6
Petr <117358265+pit133@users.noreply.github.com>
2026-01-23 23:37:02 +0700

    Add modal window menu for BurgerButton

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -17,42 +17,44 @@ const Header = (props) => {
   return (<header className="header">
     <div className="header__inner container">
       <Logo loading="eager" />
-      <nav className="header__menu">
-        <ul className="header__menu-list">
-          {menuItems.map(({label, href}, index) => (
-            <li
-              key={index}
-              className="header__menu-item"
-            >
-              <a
-                className={classNames("header__menu-link", {
-                  'is-active': href === url,
-                })}
-                href={href}
+      <dialog className="header__overlay-menu-dialog">
+        <nav className="header__menu">
+          <ul className="header__menu-list">
+            {menuItems.map(({label, href}, index) => (
+              <li
+                key={index}
+                className="header__menu-item"
               >
-                {label}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <div className="header__actions">
-        <Button
-          className='header__button'
-          label = "search"
-          mode = "transparent"
-          isLabelHidden
-          iconName='search'
-        />
-        <Button
-          className='header__button'
-          label="Notifications"
-          mode ="transparent"
-          isLabelHidden
-          iconName='notification'
-        />
-        <BurgerButton className="header__burger-button" />
-      </div>
+                <a
+                  className={classNames("header__menu-link", {
+                    'is-active': href === url,
+                  })}
+                  href={href}
+                >
+                  {label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className="header__actions">
+          <Button
+            className='header__button'
+            label="search"
+            mode="transparent"
+            isLabelHidden
+            iconName='search'
+          />
+          <Button
+            className='header__button'
+            label="Notifications"
+            mode="transparent"
+            isLabelHidden
+            iconName='notification'
+          />
+        </div>
+      </dialog>
+      <BurgerButton className="header__burger-button visible-tablet" />
     </div>
   </header>)
 }
